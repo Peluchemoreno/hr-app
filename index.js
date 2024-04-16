399189b52b7422a24f907298ef2c47e800ef0002
Justin McDonald <justin@Sharktub.homenet.com>
2024-04-15 21:39:52 -0500

    implement module for responding to client

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const employeeService = require('./lib/employees');
+const responder = require('./lib/responseGenerator')
+const staticFile = responder.staticFile(`/public`);
 
 
-http.createServer((req, res)=>{
+http.createServer((req, res) => {
   let _url; //a parsed url in case there are parameters
 
 
@@ -10,44 +12,45 @@ http.createServer((req, res)=>{
   req.method = req.method.toUpperCase();
   console.log(`${req.method} ${req.url}`)
 
-  if (req.method !== "GET"){
-    res.writeHead(501, {'Content-Type': 'text/plain'});
+  if (req.method !== "GET") {
+    res.writeHead(501, { 'Content-Type': 'text/plain' });
     return res.end(`${req.method} is not implemented by this server`)
   }
 
-  if (_url = /^\/employees$/i.exec(req.url)){
-    employeeService.getEmployees((error, data)=>{
+  
+
+//_url = /^\/employees$/i.exec(req.url)
+
+  if (_url = /^\/employees$/i.exec(req.url)) {
+    employeeService.getEmployees((error, data) => {
       if (error) {
-        res.writeHead(500);
-        res.end(`there was an error ${error}`)
-      } 
-
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(data))
-
-    })
-    // res.writeHead(200);
-    // return res.end()
-  } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)){
-    employeeService.getEmployee(_url[1], (error, data)=>{
-      if (error){
-        res.writeHead(500);
-        res.end(`There was an error ${error}`)
-      } else if (!data){
-        res.writeHead(404);
-        res.end(`NO such resource`)
+        responder.send500(error, res)
       }
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end(JSON.stringify(data))
+
+      responder.sendJson(data, res)
+
     })
-    res.writeHead(200)
-    return res.end('a single employee')
+
+// _url = /^\/employees\/(\d+)$/i.exec(req.url)
+
+  } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
+    employeeService.getEmployee(_url[1], (error, data) => {
+      if (error) {
+        responder.send500(error, res)
+      } else if (!data) {
+        responder.send404(res)
+      }
+      responder.sendJson(data, res)
+    })
+    
   } else {
     res.writeHead(200)
-    res.end('a static file')
+
+    staticFile('/home.html', res)
   }
 
   // res.end(`The current time is ${Date.now()}`)
 }).listen(8080, "127.0.0.1")
 
 console.log(`The server is running at http://127.0.0.1:8080/`)
+
